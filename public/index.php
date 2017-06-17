b9fcbaccd0b79878452c2cf371dd668430923211
Joe <joe@xxtime.com>
2017-06-17 09:31:16 +0800

    去冗余 & 格式化

--- public/index.php
+++ public/index.php
@@ -1,18 +1,15 @@
 <?php
 
 /**
- * Link: https://github.com/phalcon/mvc
+ * @link:   https://github.com/phalcon/mvc
+ * @link:   https://docs.phalconphp.com/zh/latest/index.html
  */
-
-define('BASE_DIR', dirname(__DIR__));
-define('APP_DIR', BASE_DIR . '/app');
 
 use Phalcon\Mvc\Application;
 
+define('BASE_DIR', dirname(__DIR__));
+define('APP_DIR', BASE_DIR . '/app');
 include APP_DIR . "/bootstrap/loader.php";
-
 include APP_DIR . "/bootstrap/services.php";
-
 $application = new Application($di);
-
 echo $application->handle()->getContent();
